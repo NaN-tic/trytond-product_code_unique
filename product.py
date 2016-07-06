be6dc86cff1bece60bd200c4965266f46a9c2d38
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-06 11:42:05 +0200

    Fix sql constraints

--- product.py
+++ product.py
@@ -1,19 +1,21 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
+from trytond.model import Unique
 from trytond.pool import PoolMeta
 
 __all__ = ['Product']
-__metaclass__ = PoolMeta
 
 
 class Product:
+    __metaclass__ = PoolMeta
     __name__ = 'product.product'
 
     @classmethod
     def __setup__(cls):
         super(Product, cls).__setup__()
+        t = cls.__table__()
         cls._sql_constraints += [
-            ('code_uniq', 'UNIQUE (code)',
+            ('code_uniq', Unique(t, t.code),
                 'The Code of the Product must be unique.'),
             ]
 
