5a57357d425b198e55f1261201b00267ff1740fd
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-12 13:17:36 +0200

    Update test to version 4.0

--- tests/test_product_code_unique.py
+++ tests/test_product_code_unique.py
@@ -1,60 +1,62 @@
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
 import unittest
-import trytond.tests.test_tryton
 from decimal import Decimal
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
+
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
 from trytond.transaction import Transaction
+from trytond.pool import Pool
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
+    module = 'product_code_unique'
 
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('product_code_unique')
-        self.template = POOL.get('product.template')
-        self.uom = POOL.get('product.uom')
-
+    @with_transaction()
     def test0010check_uniqueness(self):
         'Test check uniqueness'
-        with Transaction().start(DB_NAME, USER,
-                context=CONTEXT) as transaction:
-            kilogram, = self.uom.search([
-                    ('name', '=', 'Kilogram'),
-                    ], limit=1)
-            self.template.create([{
-                        'name': 'P1',
-                        'type': 'goods',
-                        'list_price': Decimal(20),
-                        'cost_price': Decimal(10),
-                        'default_uom': kilogram.id,
-                        'products': [('create', [{
-                                        'code': '1',
-                                        }])],
-                        }])
+        pool = Pool()
+        Template = pool.get('product.template')
+        Uom = pool.get('product.uom')
+        transaction = Transaction()
 
-            # Don't fail if no code
-            self.template.create([{
-                        'name': 'P2',
-                        'type': 'goods',
-                        'list_price': Decimal(20),
-                        'cost_price': Decimal(10),
-                        'default_uom': kilogram.id,
-                        'products': [('create', [{}])],
-                        }])
+        kilogram, = Uom.search([
+                ('name', '=', 'Kilogram'),
+                ], limit=1)
+        Template.create([{
+                    'name': 'P1',
+                    'type': 'goods',
+                    'list_price': Decimal(20),
+                    'cost_price': Decimal(10),
+                    'default_uom': kilogram.id,
+                    'products': [('create', [{
+                                    'code': '1',
+                                    }])],
+                    }])
 
-            # Fail if repeated code
-            self.assertRaises(Exception, self.template.create, [{
-                        'name': 'P3',
-                        'type': 'goods',
-                        'list_price': Decimal(20),
-                        'cost_price': Decimal(10),
-                        'default_uom': kilogram.id,
-                        'products': [('create', [{
-                                        'code': '1',
-                                        }])],
-                        }])
-            transaction.cursor.rollback()
+        # Don't fail if no code
+        Template.create([{
+                    'name': 'P2',
+                    'type': 'goods',
+                    'list_price': Decimal(20),
+                    'cost_price': Decimal(10),
+                    'default_uom': kilogram.id,
+                    'products': [('create', [{}])],
+                    }])
+
+        # Fail if repeated code
+        self.assertRaises(Exception, Template.create, [{
+                    'name': 'P3',
+                    'type': 'goods',
+                    'list_price': Decimal(20),
+                    'cost_price': Decimal(10),
+                    'default_uom': kilogram.id,
+                    'products': [('create', [{
+                                    'code': '1',
+                                    }])],
+                    }])
+        transaction.rollback()
 
 
 def suite():
