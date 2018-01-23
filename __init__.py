97cb353150d7b227e63d1d45003d62d57277279b
C?dric Krier <ced@b2ck.com>
2018-01-23 17:41:12 +0100

    Update to 4.7

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .product import *
+from . import product
 
 
 def register():
     Pool.register(
-        Product,
+        product.Product,
         module='product_code_unique', type_='model')
